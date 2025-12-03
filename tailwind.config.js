aec82c768d4539b31f3cc742f476028237533447
lassewehlmanngit <lassewehlmann.business@gmail.comgit config --global user.namegit config --global user.emailcdgit config --global user.name Your>
2025-12-03 14:36:01 +0900

    style: increase container width to 1280px

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,8 +17,8 @@ export default {
         sm: '640px',
         md: '768px',
         lg: '1024px',
-        xl: '1100px',
-        '2xl': '1100px',
+        xl: '1280px',
+        '2xl': '1280px',
       },
     },
     extend: {
